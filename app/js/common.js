9ae3335ee952adb12b9fcb1ce1ddcf45382f8569
AlexLavrenchuk <Alex.Lavrenchuk87@gmail.com>
2018-10-23 00:09:12 +0300

    version without adaptability and lower section

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-	$('.slider').bxSlider();
+  
+	$('.slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 2,
+    arrows: false,
+		dots: true,
+	  	dotsClass: "my-dots",
+
+	  	 responsive: [
+    {
+      breakpoint: 993,
+      settings: {
+        slidesToShow: 3,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+      }
+    }
+  ]
+	});
 });
